b77dfe847a61c29d5908f202f9c23fd3254788be
perogeremmer <mhudyaramadhana@gmail.com>
2023-12-19 13:13:31 +0700

    Add new tutorial and sample automation testing

---
+++ Komandro/IoT/material/sample/6-automation-testing/services/invoice_service.py
@@ -0,0 +1,27 @@
+from services.base_service import BaseService
+from cores.broker import Broker
+from constants.states import AppStates
+from time import sleep
+from cores.app_log import AppLogger
+
+class InvoiceService(BaseService):
+    allowed_states = [AppStates.ORDER_CANCELED.value, AppStates.ORDER_PAID.value]
+
+    def __init__(self, broker: Broker):
+        self.broker = broker
+        self.logger = AppLogger()
+
+    def execute(self):
+        state = self.broker.get_state()
+        if state not in self.allowed_states:
+            return False
+
+        self.logger.info(f"Invoice service started. State: {self}")
+
+        # Do code here
+        sleep(3)
+        # Sample sleep 3 seconds to pretend like code do something
+
+        new_state = AppStates.IDLE.value
+        self.broker.set_state(new_state)
+        self.logger.info(f"Change state into: {new_state}")
